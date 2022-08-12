5589ccef8ca26606423f31935a525f427e2b0e63
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-11 20:31:41 -0700

    Use baker to save.

--- addons/miku_motion_bake/runtime/VMDBake.gd
+++ addons/miku_motion_bake/runtime/VMDBake.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-export(String, FILE) var import_vrm_path = "res://addons/vrm/import_vrm.gd"
+var import_vrm_path : String= "res://addons/vrm/import_vrm.gd"
 export(String, FILE) var motion_path
 export(String, FILE) var model_path
 
@@ -9,11 +9,8 @@ func _ready():
 	var animator: VRMAnimatorBake
 	var VRMImport = load(import_vrm_path)
 	var model_instance: Spatial
-	if model_path.begins_with("res://"):
-		model_instance = load(model_path).instance()
-	else:
-		var vrm_loader = load("res://addons/vrm/vrm_loader.gd").new()
-		model_instance = vrm_loader.import_scene(model_path, 1, 1000)
+	var vrm_loader = load("res://addons/vrm/vrm_loader.gd").new()
+	model_instance = vrm_loader.import_scene(model_path, 1, 1000)
 	model_instance.rotate_y(deg2rad(180))
 	animator = VRMAnimatorBake.new()
 	vmd_player = VMDPlayerBake.new()
@@ -29,10 +26,9 @@ func _ready():
 		var new_animation_player : AnimationPlayer= model_instance.get_node("anim")
 		for key_i in anims.keys():
 			new_animation_player.add_animation(key_i, anims[key_i])
-		var scene : PackedScene = PackedScene.new()
-		scene.pack(model_instance)
-		ResourceSaver.save("res://.import/%s_%s.scn" % [model_path.get_file().get_basename(), path.get_file().get_basename()], 
-			scene, ResourceSaver.FLAG_COMPRESS)
+		var convert_gltf2 = PackedSceneGLTF.new()
+		var filename : String = "user://%s_%s.glb" % [model_path.get_file().get_basename(), path.get_file().get_basename()]
+		convert_gltf2.export_gltf(model_instance, filename, 0, 30.0)
 
 
 func _process(_delta):
